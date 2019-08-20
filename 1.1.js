f069dd0794e7edc56e086e164664b65843ed7fc0
Marah Andrioli <marahandrioli@gmail.com>
2019-08-20 15:44:03 +0200

    ajout exercices 1 et 2

---
+++ 1.1.js
@@ -0,0 +1,4 @@
+const readlineSync = require("readline-sync");
+
+let name = "Chicorne";
+console.log(name);
---
+++ 1.2.js
@@ -0,0 +1,6 @@
+const readlineSync = require("readline-sync");
+
+let name = "Lecinquièmemousquetaire";
+let firstName = "Albert";
+let city = "Paris";
+console.log ("Your name is " + firstName + " " + name + " and you live in " + city +".");
